003b6d09ae0286e645cf5b6d45ed523eb7482ea1
jarffery <jerrylee0128@gmail.com>
2025-02-04 11:18:18 -0800

    setup lua folder

---
+++ lua/plugins.lua
@@ -0,0 +1,79 @@
+-- 自动安装 Packer.nvim
+-- 插件安装目录
+-- ~/.local/share/nvim/site/pack/packer/
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local paccker_bootstrap
+if fn.empty(fn.glob(install_path)) > 0 then
+  vim.notify("正在安装Pakcer.nvim，请稍后...")
+  paccker_bootstrap = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    -- "https://gitcode.net/mirrors/wbthomason/packer.nvim",
+    install_path,
+  })
+
+  -- https://github.com/wbthomason/packer.nvim/issues/750
+  local rtp_addition = vim.fn.stdpath("data") .. "/site/pack/*/start/*"
+  if not string.find(vim.o.runtimepath, rtp_addition) then
+    vim.o.runtimepath = rtp_addition .. "," .. vim.o.runtimepath
+  end
+  vim.notify("Pakcer.nvim 安装完毕")
+end
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  vim.notify("没有安装 packer.nvim")
+  return
+end
+
+packer.startup({
+  function(use)
+    -- Packer 可以升级自己
+    use("wbthomason/packer.nvim")
+    --------------------- colorschemes --------------------
+    -- tokyonight
+    use("folke/tokyonight.nvim")
+    -- OceanicNext
+    use("mhartington/oceanic-next")
+    -- gruvbox
+    use({ "ellisonleao/gruvbox.nvim", requires = { "rktjmp/lush.nvim" } })
+    -- nord
+    use("shaunsingh/nord.nvim")
+    -- onedark
+    use("ful1e5/onedark.nvim")
+    -- nightfox
+    use("EdenEast/nightfox.nvim")
+    -- nomokai
+    use("tanvirtin/monokai.nvim")
+    -------------------------------------------------------
+    -------------------------- plugins --------------------
+    -- nvim-tree (新增)
+    use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })  
+    -------------------------------------------------------
+  end,
+  config = {
+     display = {
+    -- 使用浮动窗口显示
+       open_fn = function()
+         return require("packer.util").float({ border = "single" })
+       end,
+     },
+  },
+})
+
+-- 每次保存 plugins.lua 自动安装插件
+-- move to autocmds.lua
+pcall(
+  vim.cmd,
+  [[
+    augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+  ]]
+)
