061337bd2a03f2b428072466aaef4e3d511af2e5
jarffery <jerrylee0128@gmail.com>
2025-02-04 16:32:11 -0800

    init.lua updated

--- init.lua
+++ init.lua
@@ -1,5 +1,15 @@
+-- disable netrew at the very start
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+-- load the required plugins
+-- 基础配置
 require("basic")
+-- keymapping
 require("keybindings")
-require("plugins")
+-- colormap setup
 require("colorscheme")
+-- plugins setup
+require("plugins")
 require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
