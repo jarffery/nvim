a8a92d3a9d7f2b2f70fc56ecf5c11bbeedb9212d
Jarffery <jerrylee0128@gmail.com>
2025-02-10 09:45:25 -0800

    embed the vscode in the init.lua

--- init.lua
+++ init.lua
@@ -1,23 +1,26 @@
--- disable netrew at the very start
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
--- load the required plugins
+if vim.g.vscode then
+    --VSCode extension
+else
+    -- disable netrew at the very start
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    -- load the required plugins
 
--- plugins setup
-require("plugins")
-require("plugin-config.nvim-tree")
-require("plugin-config.bufferline")
-require("plugin-config.lualine")
-require("plugin-config.telescope")
-require("plugin-config.dashboard")
-require("plugin-config.project")
-require("plugin-config.nvim-treesitter")
--- LSP
---require("lsp")
--- colormap setup
-require("colorscheme")
--- 基础配置
-require("basic")
--- keymapping
-require("keybindings")
-
+    -- plugins setup
+    require("plugins")
+    require("plugin-config.nvim-tree")
+    require("plugin-config.bufferline")
+    require("plugin-config.lualine")
+    require("plugin-config.telescope")
+    require("plugin-config.dashboard")
+    require("plugin-config.project")
+    require("plugin-config.nvim-treesitter")
+    -- LSP
+    --require("lsp")
+    -- colormap setup
+    require("colorscheme")
+    -- 基础配置
+    require("basic")
+    -- keymapping
+    require("keybindings")
+end
