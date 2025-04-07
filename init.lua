b1e1c15b630eb1451edeef81a5cd310af21f8e01
jarffery <jerrylee0128@gmail.com>
2025-04-07 10:53:55 -0700

    update the init.lua setup and remove the vscode setting

--- init.lua
+++ init.lua
@@ -1,30 +1,17 @@
-if vim.g.vscode then
-    --VSCode extension
-    -- 基础配置
-    require("basic_vscode")
-    -- keymapping
-    --plugins setup
-else
-    -- disable netrew at the very start
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-    -- load the required plugins
-
-    -- plugins setup
-    require("plugins")
-    require("plugin-config.nvim-tree")
-    require("plugin-config.bufferline")
-    require("plugin-config.lualine")
-    require("plugin-config.telescope")
-    require("plugin-config.dashboard")
-    require("plugin-config.project")
-    require("plugin-config.nvim-treesitter")
-    -- LSP
-    --require("lsp")
-    -- colormap setup
-    require("colorscheme")
-    -- 基础配置
-    require("basic")
-    -- keymapping
-    require("keybindings")
-end
+-- plugins setup
+require("plugins")
+require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
+require("plugin-config.telescope")
+require("plugin-config.dashboard")
+require("plugin-config.project")
+require("plugin-config.nvim-treesitter")
+-- LSP
+--require("lsp")
+-- colormap setup
+require("colorscheme")
+-- 基础配置
+require("basic")
+-- keymapping
+require("keybindings")
